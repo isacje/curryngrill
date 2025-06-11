9f246c7a8783ec5ee3dce28740e048dda55b5dd7
isacje <isaceralil@gmail.com>
2025-06-10 22:10:35 -0400

    updated on 10-16-2025

--- script.js
+++ script.js
@@ -286,3 +286,18 @@ document.addEventListener("mousemove", (e) => {
       layer.style.transform = `translate(${x * depth}px, ${y * depth}px)`;
     });
   });
+
+let lastScroll = 0;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > lastScroll) {
+    // Scrolling down - hide header
+    header.style.transform = 'translateY(-100%)';
+  } else {
+    // Scrolling up - show header
+    header.style.transform = 'translateY(0)';
+  }
+  lastScroll = currentScroll;
+});
